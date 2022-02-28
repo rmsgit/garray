083aafb9f1a6d29adc68c002fe8b1741fad64f96
Ruwan Madusanaka <ruwanm@99x.lk>
2022-02-28 12:39:44 +0530

    Map function

--- map.go
+++ map.go
@@ -1,5 +1,10 @@
 package garrays
 
-func main() {
+func Map[T any, E any](array []T, fn func(element T, index int) E) *[]E {
+	returnArray := make([]E, 0)
+	for i, element := range array {
+		returnArray = append(returnArray, fn(element, i))
+	}
 
+	return &returnArray
 }
